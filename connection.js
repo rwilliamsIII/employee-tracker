89543c43b501cf3ff1b20134b83ee078e86f65b6
Robert Williams <robert@a3marketing.com>
2020-04-28 17:56:45 -0500

    created mysql connection and env file to hide sql password

---
+++ connection.js
@@ -0,0 +1,12 @@
+const mysql = require("mysql");
+require("dotenv").config();
+
+const connection = mysql.createConnection({
+    host: "localhost",
+    port: 5000,
+    user: "root",
+    password: process.env.PASSWORD,
+    database: "employee_db"
+});
+
+module.exports = connection;
